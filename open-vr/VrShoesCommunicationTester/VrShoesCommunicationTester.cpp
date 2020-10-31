e43a8044470b18012d855c86ec2ac684622d66f8
Alexander Evans <alex@evansalex.com>
2020-10-31 10:51:49 -0500

    Updated tester to start the walking algorithm.

--- open-vr/VrShoesCommunicationTester/VrShoesCommunicationTester.cpp
+++ open-vr/VrShoesCommunicationTester/VrShoesCommunicationTester.cpp
@@ -58,6 +58,8 @@ int main()
     VrShoe prevVrShoe1 = VrShoe();
     VrShoe prevVrShoe2 = VrShoe();
 
+    communicator->startAlgorithm(vrShoe1->deviceId);
+    communicator->startAlgorithm(vrShoe2->deviceId);
     while (true)
     {
         communicator->processMessages();
